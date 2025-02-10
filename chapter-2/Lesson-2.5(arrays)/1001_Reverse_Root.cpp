b12e72973d038b17b9488043c91098aeca4f0422
naimurnemu <naimurrahaman82@gmail.com>
2025-02-10 17:41:45 +0600

    changes in arrays

---
+++ chapter-2/Lesson-2.5(arrays)/1001_Reverse_Root.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios:: sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  long long int item;
+  while (cin >> item) 
+  {
+    cout << fixed << setprecision(4) << sqrt(item) << endl;
+  }
+  
+  
+  return 0;
+}
